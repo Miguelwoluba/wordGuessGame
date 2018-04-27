6dde78d9dbfe347985274bf93c151e9e2dbce56a
miguel mejia <mejiamiguel4@gmail.com>
2018-04-26 19:00:03 -0600

    scafoldding and other suff done

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    var wordBank = ["wobalubbadubdub", "rick", "morty", "beth","summer", "Abradolf Lincler", "Anatomy Park", "Bird Person", "Cognition Amplifier", "Dream Inceptors", "Fart", "Galactic Federation", "Gazorpians", "Gwendolyn", "Ice T", "Interdimensional Cable", "Jerryboree", "jerry",
+        "Mr. Meeseeks", "Microverse Battery", "Needful Things", "Operation Phoenix", "Planet Music", "Mr. Poopybutthole", "Portal Gun", "Roy Videogame", "Scary Terry", "Shleemypants", "Squanchy", "Unity", "the vindicators"];
+         
+
+
+
+}
